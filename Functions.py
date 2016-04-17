e0b89e1c479b044af97d30f31bace9ed5dd0bed1
Obatola Seward-Evans <oba.seward@gmail.com>
2016-04-17 19:06:44 -0400

    Initial commit

---
+++ Functions.py
@@ -0,0 +1,17 @@
+# Obatola Seward-Evans
+# CS 2223 Project 2
+# April 17, 2016
+
+# use Run_Fib.py brute force and recursive functions
+# import numpy
+import time
+
+# - - - "BRUTE FORCE" FUNCTION - - -
+def getMatrix():
+    # matrix = numpy.zeros((width, height))
+    matrix = {} # create matrix of dynamic size
+
+
+
+    matrix[1000,2000] = 15
+    return matrix[1000,2000]
---
+++ Run_Matrix_Cost.py
@@ -0,0 +1,43 @@
+# Obatola Seward-Evans
+# CS 2223 Project 2
+# April 17, 2016
+
+# The main function. asks for the user if he/she would like to run the exhastive search or 
+# a greedy algorithm 
+# The function then runs either exhastiveSearch() or greedyAlgorithm() which can be found in 
+# Functions.py. For both functions the time elapsed is printed
+# [relies on the 'exhastiveSearch' and 'greedyAlgorithm' in the file Functions.py]
+
+#!/usr/bin/python
+
+# import the actual functions from Functions.py:
+from Functions import *
+
+while True:
+    print "\n\n- - - MAIN MENU - - - "
+    print "Select function:"
+    print "  1. exhastive search"
+    print "  2. greedy algorithm"
+    print "  3. quit"
+    option = input("\n> ") # gather option value from user.
+
+    # run BRUTE FORCE function if user selects 1st option:
+    if option == 1:
+        print "\n\n- - - EXHAUSTIVE SEARCH ALGORITHM - - - "
+        print "given matrix is: \n", getMatrix()
+
+
+    # run RECURSIVE function if user selects 1st option:
+    elif option == 2:
+        print "\n\n- - - GREEDY ALGORITHM - - - "
+
+    elif option == 3:
+        print "\n\nIt's sad to see you go :(\n"
+        break # end while loop and quit
+
+    else:
+        print "\nWrong input dude... Let's this try again :D"
+
+    time.sleep(3) # wait 4 seconds before showing main menu again
+
+
