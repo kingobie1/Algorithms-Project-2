bae263866711c6f80b060bc6d29675f16952ed99
Obatola Seward-Evans <oba.seward@gmail.com>
2016-04-18 07:59:01 -0400

    Starting greedy algorithm

--- Functions.py
+++ Functions.py
@@ -5,22 +5,35 @@
 # use Run_Fib.py brute force and recursive functions
 # import numpy
 import time
+matrix = {} # create matrix of dynamic size
 
 # - - - "BRUTE FORCE" FUNCTION - - -
 def getMatrix(width, height):
     # matrix = numpy.zeros((width, height))
-    matrix = {} # create matrix of dynamic size
+    # matrix = {} # create matrix of dynamic size
     i = 0
     j = 0
 
     matrixFile = open("matrix", "r")
 
-    for i in range(0, height):
-        for j in range(0, width):
-            str = matrixFile.read(2)
-            str = ''.join(str.split()) # take out space and newline
-            matrix[i, j] = int(str) # convert to integer and add to matrix
-            print matrix[i, j]
+    for h in range(0, height):
+        str = matrixFile.readline()
+        tempArray = str.split()
+        for w in range(0, width):
+            # str = matrixFile.read(2)
+            # str = ''.join(str.split()) # take out space and newline
+            # matrix[i, j] = int(str) # convert to integer and add to matrix
+            matrix[h, w] = int(tempArray[w]) # convert to integer and add to matrix
+            print matrix[h, w]
 
     matrixFile.close()
     return matrix
+
+# def greedyAlgorithm(width, height):
+#     minForRow
+#     totalCost
+#     takenColumns=[0 for i in range(width)] # means taken
+
+#     for i in range(0, width)
+#         print takenColumns[i]
+
